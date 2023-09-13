f0f59548a52ca439f88d4f2f8f95a2e8c6921d6d
ojedam13 <80652770+ojedam13@users.noreply.github.com>
2023-09-13 20:42:22 -0300

    Clase 01 de Python
    Esta es la semana 01 de python

---
+++ PYTHON/Semana_01/Clase01/Sistemas_numeracion.py
@@ -0,0 +1,58 @@
+# 2.1 Comenzamos con -> Decimal, Binario, Octal y Hexadecimal Alumno: Giuliana Paola Diaz Luna
+# Profundizando en los sistemas de numeración
+
+# Sistema decimal
+a = 10
+print (f'a decimal:{a}')
+
+# Sistema binario ( manejamos el mismo numero pero con una literal diferente)
+a = 0b1010
+print (f'a binario:{a}')
+
+# Sistema Octal
+a = 0o12
+print (f'a octal:{a}')
+
+# Sistema hexadecimal
+a = 0xA
+print (f'a hexadecimal:{a}')
+
+# 2.2 Seguimos con 
+# -> Decimal, Binario, Octal y Hexadecimal
+#  Ahora con int('valor', base) Alumno : Miguel Rodriguez Saquilan
+
+
+
+# Base decimal
+a = int('23', 10)
+print(f'a = 10 Base decimal:{a}');
+
+# Base Binaria
+a = int('10111', 2)
+print(f'a = 2 binaria:{a}');
+
+# base Octal
+a = int('12', 8)
+print(f'a = 8 Base octal:{a}');
+
+# base Hexadecimal
+a = int('A', 16)
+print(f'a = 10 Base Hexadecimal:{a}');
+
+# Base 5 sua valores son 0,1,2,3,4
+a = int('344', 5)
+print(f'a =5 base Base 5 :{a}');
+
+# 2.4 Comenzamos con -> 2.4 Exponencial negativo y positivo  Alumno: Marcelo Alejandro Quispe
+
+a = 3e5
+print(f'a: {a:.2f}')
+
+
+a = 3e-5
+print(f'a: {a:.5f}')
+
+# Cualquier calculo que incluye un float , todo cambia a float
+a = 4.0 + 5
+print(a)
+print(type(a))
