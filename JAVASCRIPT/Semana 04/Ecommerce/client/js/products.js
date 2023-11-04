4c9ea63ca9e88187d6862e79d7c9c74defa70488
Axel Fornerod <Fornerodaxel@hotmail.com>
2023-11-04 14:30:54 -0300

    Leccion 04 completa

---
+++ JAVASCRIPT/Semana 04/Ecommerce/client/js/products.js
@@ -0,0 +1,35 @@
+const productos =[
+    {
+        id: 1,
+        productName: "Martillo",
+        price: 480,
+        quanty: 1,
+        img: "../media/Taladro.jpg",
+
+    },
+    {
+        id: 2,
+        productName: "Destornilladores",
+        price: 100,
+        quanty: 1,
+        img: "../media/Destornilladores.jpg",
+
+    },
+    {
+        id: 3,
+        productName: "Caja De Herramientas",
+        price: 700,
+        quanty: 1,
+        img: "../media/CajaDeHerramientas.jpg",
+
+    },
+    {
+        id: 4,
+        productName: "Amoladora",
+        price: 300,
+        quanty: 1,
+        img: "../media/Amoladora.png",
+
+    },
+
+]
