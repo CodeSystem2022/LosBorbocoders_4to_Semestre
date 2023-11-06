2f860b49ccca17d96ca5a30e922c45ee39370f5b
knlc01 <kevinlopez00001@gmail.com>
2023-11-06 14:44:03 -0300

    semana05 js

---
+++ JAVASCRIPT/Semana 05/src/controllers/auth.controller.js
@@ -0,0 +1,6 @@
+
+export const signin = (res, res) => res.send("ingresando");
+export const signup = (res, res) => res.send("registrando");
+export const signout = (res, res) => res.send("cerrando sesion");
+export const profile = (res, res) => res.send("Perfil de usuario");
+
---
+++ JAVASCRIPT/Semana 05/src/controllers/tareas.controller.js
@@ -0,0 +1,5 @@
+export const listarTareas = (res, res) => res.send("obteniendo Tareas");
+export const listarTares = (res, res) => res.send("obteniendo Tareas Unica");
+export const crearTarea = (res, res) => res.send("Creando Tarea");
+export const actualizarTarea = (res, res) => res.send("Actualizando Tarea Unica");
+export const eliminarTarea = (res, res) => res.send("Eliminando Tarea Unica");
