ff5d76f69e380a4680e1d6aca3d266f1d866b753
Javi Gomez Allosa <106932015+javigoall@users.noreply.github.com>
2023-11-09 16:51:40 -0300

    Semana 02
    Semana 02 python

---
+++ PYTHON/Semana 02/tipo_float.py
@@ -0,0 +1,23 @@
+
+a = int('23', 10)
+print(f'a = base decimal: {a}')
+
+a = int('10111', 2)
+print(f'a = base binario: {a}')
+a = 3.0
+
+a = float(10) 
+a = float('10') 
+print(f'a: {a:.2f}')
+
+a = 3e5 
+print(f'a: {a}')
+
+a = 3e-5
+print(f'a: {a:.5f}')
+print(f'a: {a:.9f}')
+
+a = 4.0 + 5
+print(a)
+print(type(a))
+
