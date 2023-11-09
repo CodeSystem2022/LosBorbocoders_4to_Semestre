a9ccd8471a629f6541de3c53dac42b0a62aef562
Axel Fornerod <Fornerodaxel@hotmail.com>
2023-11-09 17:15:18 -0300

    Semana 11 NODE JS

---
+++ NODE JS/Semana 11/asyncAwait.js
@@ -0,0 +1,79 @@
+async function hola(nombre) { 
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            console.log('Hola ' + nombre);
+            resolve(nombre);
+        }, 1000);
+    })
+}
+
+async function hablar(nombre) {
+    return new Promise((resolve, reject) => { 
+        setTimeout(function () {
+            console.log('bla bla bla');
+            resolve(nombre);
+        }, 1000);
+    });
+}
+
+async function adios(nombre) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            console.log('Adiós ' + nombre);
+            resolve();
+        }, 1000);
+    });
+}
+
+async function main() {
+    let nombre = await hola('Ariel');
+    await hablar();
+    await hablar();
+    await hablar();
+    await adios(nombre);
+    console.log('Termina el proceso...');
+
+}
+
+main();
+
+function sayHello(name) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            console.log('Hello ' + name);
+            resolve(name);
+        }, 1000);
+    });
+}
+
+function talk(name) {
+    return new Promise((resolve, reject) => { 
+        setTimeout(() => {
+            console.log('bla bla bla');
+            resolve(name);
+        }, 1000);
+    });
+}
+
+function sayBye(name) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Goodbye ' + name);
+            resolve(name);
+        }, 1000);
+    });
+}
+
+async function conversation(name) {
+    console.log('Code in Eenglish');
+    console.log('Starting async process...');
+    await sayHello(name);
+    await talk();
+    await talk();
+    await talk();
+    await sayBye(name);
+    console.log('Process completed')
+
+}
+
+conversation('Ariel');
