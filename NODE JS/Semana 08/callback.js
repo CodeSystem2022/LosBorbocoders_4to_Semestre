e42b06c2413cd3725f848188c7db3b1c610d7aed
Javi Gomez Allosa <106932015+javigoall@users.noreply.github.com>
2023-11-09 11:49:40 -0300

    Create callback.js

---
+++ NODE JS/Semana 08/callback.js
@@ -0,0 +1,25 @@
+// - Gomez Allosa Victor Javier --- > Semana 08 Node.
+function hola(nombre, miCallback){
+    setTimeout( function() {
+        console.log('Hola ' + nombre);
+        miCallback(nombre);
+    }, 1000);
+}
+
+//Función asincrona - adios
+function adios(nombre, otrocallback) {
+    setTimeout(function(){
+        console.log('Adiós '+ nombre);
+        otrocallback();
+    }, 1500);   
+}
+
+console.log('Iniciando el proceo...');
+
+hola('Carlos', function(nombre) {
+    adios(nombre, function(){
+        console.log('Terminando el proceso...');    
+    });
+});
+//hola('Carlos', function(){});
+//adios('Carlos', function(){});
